be588e52b4bac0039114ad564e0fc3f90f11785f
Rodrigo Suarez <ing.rsuarez@gmail.com>
2022-11-23 09:20:17 -0300

    Login feature initial stage

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/bright';
+    public $baseURL = 'http://bright.admesys.com';
 
     /**
      * --------------------------------------------------------------------------
